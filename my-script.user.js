826d537a745daff8ee6542c82c3d1ed23b80f80f
b-rad57-pixel <b-rad57@live.com>
2026-01-14 15:30:18 +0800

    Update my-script.user.js

--- my-script.user.js
+++ my-script.user.js
@@ -9,8 +9,8 @@
 // @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @updateURL    https://raw.githubusercontent.com/b-rad57-pixel/tampermonkey-scripts/main/my-script.user.js
-// @downloadURL  https://raw.githubusercontent.com/b-rad57-pixel/tampermonkey-scripts/main/my-script.user.js
+// @updateURL    https://b-rad57-pixel.github.io/tampermonkey-scripts/my-script.user.js
+// @downloadURL  https://b-rad57-pixel.github.io/tampermonkey-scripts/my-script.user.js
 // ==/UserScript==
 
 
